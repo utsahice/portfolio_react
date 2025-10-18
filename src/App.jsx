f9ecf395fdf934078818be1daee62793168d012a
utsahice <utsahic.encircle@gmail.com>
2025-10-18 16:45:48 +0530

    feat : add portfolio page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+// import FunctionClick from './events';
+import Portfolio from './portfolio'
+
+function App() {
+const bio = "I’m a Full Stack Developer And Working On React Fundamentals";
+
+  return (
+    <Portfolio bio={bio}/>
+    // <FunctionClick/>
+  );
+}
+
+export default App
